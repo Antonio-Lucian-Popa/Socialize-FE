6dc8d98da419b849642db6e5ff0d3ed625ed0d3d
popa.antonio.lucian <antonio.popa.dev@gmail.com>
2024-03-18 13:40:45 +0200

    Added skeleton on discovery images

--- src/app/features/discovery/discovery.component.ts
+++ src/app/features/discovery/discovery.component.ts
@@ -12,16 +12,49 @@ export class DiscoveryComponent implements OnInit {
   popularImages = [
     {id: 1, image: 'https://images.unsplash.com/photo-1682686578842-00ba49b0a71a?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
     {id: 2, image: 'https://images.unsplash.com/photo-1682685797365-41f45b562c0a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
-    {id: 3, image: 'https://images.unsplash.com/photo-1682686578842-00ba49b0a71a?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
-    {id: 4, image: 'https://images.unsplash.com/photo-1682685797365-41f45b562c0a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
-    {id: 5, image: 'https://images.unsplash.com/photo-1682686578842-00ba49b0a71a?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
-    {id: 6, image: 'https://images.unsplash.com/photo-1682685797365-41f45b562c0a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
-
+    {id: 3, image: 'https://images.unsplash.com/photo-1707343848655-a196bfe88861?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
+    {id: 4, image: 'https://images.unsplash.com/photo-1710743385110-23d273762d14?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
+    {id: 5, image: 'https://images.unsplash.com/photo-1710596039020-b93566d8106b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'},
+    {id: 6, image: 'https://images.unsplash.com/photo-1710695174960-59026cbac187?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'}
   ];
+
+  loading: boolean = true;
+  private imagesLoaded = 0;
+  currentPage = 0;
+  totalPages = undefined; // Assume this comes from your backend
+
 
   constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.loadImages();
+  }
+
+  loadImages(): void {
+    if ((this.totalPages && this.currentPage < this.totalPages) || this.totalPages === undefined) {
+      this.loading = true;
+      // this.yourService.getImages(this.currentPage).subscribe(data => {
+      //   const newImages = data.images; // Adapt based on your actual data structure
+      //   this.totalPages = data.totalPages; // Update total pages
+      //   this.popularImages = [...this.popularImages, ...newImages];
+      //   this.currentPage++;
+      //   // Reset loading state and counter if this is a new load
+      //   this.imagesLoaded = 0;
+      //   // Don't set loading to false here; let imageLoaded handle it
+      // });
+    }
+  }
+
+  imageLoaded(): void {
+    this.imagesLoaded++;
+    if (this.imagesLoaded === this.popularImages.length) {
+      this.loading = false;
+    }
+  }
+
+  // Call this method to load more images
+  loadMore(): void {
+    this.loadImages();
   }
 
   openImageDialog(imageId: number): void {
--- src/app/features/discovery/image-view-dialog/image-view-dialog.component.ts
+++ src/app/features/discovery/image-view-dialog/image-view-dialog.component.ts
@@ -19,7 +19,21 @@ export class ImageViewDialogComponent implements OnInit {
     },
     image: 'https://plus.unsplash.com/premium_photo-1710119487367-8489287f04a9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     caption: 'This is a great image!',
-    date: '2021-04-15T19:00:00.000Z'
+    date: '2021-04-15T19:00:00.000Z',
+    likes: [
+      {
+        id: 45,
+        firstName: 'John',
+        lastName: 'Doe',
+        image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      },
+      {
+        id: 46,
+        firstName: 'Jane',
+        lastName: 'Doe',
+        image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      }
+    ]
   };
 
   comments: any[] = [
@@ -121,6 +135,16 @@ export class ImageViewDialogComponent implements OnInit {
 
   toggleLikePost(post: any): void {
     this.isPostLiked = !this.isPostLiked;
+    if(this.isPostLiked) {
+      this.popularImagePost.likes.push({
+        id: 47,
+        firstName: 'Jane',
+        lastName: 'Doe',
+        image: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+      });
+    } else {
+      this.popularImagePost.likes.splice(this.popularImagePost.likes.findIndex((like: any) => like.id === 47), 1);
+    }
     // TODO: Send a request to like/unlike the post
   }
 
